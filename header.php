22c38ea383f5e6c11571a5219d36665715a4e3fc
kimo-works <kimoforworks@gmail.com>
2022-04-10 01:55:00 +0200

    start test case

---
+++ header.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The header.
+ *
+ * This is the template that displays all of the <head> section and everything up until main.
+ * 
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
